1decc0621c2b1caa7ec8f1f32e6a950e51643380
Veeras-code <nagasai692004@gmail.com>
2025-08-15 16:53:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,6 +14,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(
     PORT,
     () => {
-        console.log(`Server is running on port ${PORT}`);
+        console.log(`Serverrrrr is running on port ${PORT}`);
     }
 );
